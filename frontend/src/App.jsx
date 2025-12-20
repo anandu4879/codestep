adccf859e66314f039b3ee4fe6117ceed5e64281
ananthakrishna KP <anandu4879@gmail.com>
2025-12-21 00:04:59 +0530

    bug fix -name issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router';
 
 import { Toaster } from 'react-hot-toast';
-import HomePage from './pages/HomePages';
+import HomePage from './pages/HomePages';//name mismatch fixed still dont get what caused it
 import ProblemsPage from './pages/ProblemsPage';
 
 
